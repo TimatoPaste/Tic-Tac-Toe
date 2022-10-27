e33bc717c302105512f8ef0b07614107a6df3882
DaKahd <90017411+DaKahd@users.noreply.github.com>
2022-10-27 15:26:43 -0700

    Create Ttt.java

---
+++ Ttt.java
@@ -0,0 +1,13 @@
+public class Ttt implements GameState{
+  boolean isGameOver(){}
+  
+  Player getWinner(){}
+  
+  Player getCurrentPlayer(){}
+  
+  ArrayList<String> getCurrentMoves(){}
+  
+  void makeMove(String move){}
+  
+  String toString(){}
+}
