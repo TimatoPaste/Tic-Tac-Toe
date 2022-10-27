b40b5dc5b16ccabd2f7b33759114e00c5f263d1b
Josh K <116848707+422331@users.noreply.github.com>
2022-10-27 15:29:50 -0700

    Update GameDriver.java

--- GameDriver.java
+++ GameDriver.java
@@ -1,24 +1,34 @@
 public class GameDriver{
+	
 	private GameState state;
-	public GameDriver(GameState initial){state = initial;}
+	
+	public GameDriver(GameState initial){
+		state = initial;
+	}
+	
 	public void play(){
 		// System.out.println("playing");
 		//part(b)
 		//base case
-		if(state.isGameover()){
+		
+		if(state.isGameover()){ //checks for winner or a draw
 			if(state.getWinner()!=null)
 				System.out.println(state.getWinner()+" wins!");
 			else 
 				System.out.println("Game ends in a draw");
 			return;
 		}
+		
 		Player current = state.getCurrentPlayer();
 		System.out.println("current player is "+current);
 		String nextMove = current.getNextMove(state);
-		if(nextMove.equals("no move"))
+		
+		if(nextMove.equals("no move")){
 			System.out.println("Player "+current+" has no move");
-		else
+		}
+		else{
 			System.out.println("Player "+current +" makes move "+nextMove);
+		}
 		state.makeMove(nextMove);
 		//recur
 		play();
