7c5dc16d827c22b88b0f6376770fa25c302c71b1
DaKahd <90017411+DaKahd@users.noreply.github.com>
2022-10-31 15:13:59 -0700

    Update Ttt.java

--- Ttt.java
+++ Ttt.java
@@ -1,13 +1,89 @@
 public class Ttt implements GameState{
-  boolean isGameOver(){}
+  private String[][] state = {
+    {" ", " ", " "}, 
+    {" ", " ", " "},
+    {" ", " ", " "}
+  };
+  private Player x;
+  private Player o;
+  private Player current;
+  public Ttt(Player x, Player o){
+    this.x = x;
+    this.o = o;
+    current = x;
+  }
+  boolean isGameOver(){
+
+  }
   
-  Player getWinner(){}
+  Player getWinner(){
+    //it is impossible to win without either having [0, 0], [1, 1], or [2, 2]
+    //three ways to win: diagonal, vertical, horizontal
+    Player potentialWinner = fromString(0, 0);
+    if(potentialWinner != null){
+      if(potentialWinner == fromString(1, 1) && potentialWinner ==fromString(2, 2))
+        return potentialWinner;//diagonal
+      if(potentialWinner == fromString(0, 1) && potentialWinner == fromString(0, 2))
+        return potentialWinner;//vertical
+      if(potentialWinner == fromString(1, 0)&& potentialWinner == fromString(2, 0))
+        return potentialWinner;//horizontal
+    }
+    if(potentialWinner != null){
+      potentialWinner = fromString(1, 1);
+      if(potentialWinner == fromString(2, 0) && potentialWinner ==fromString(0, 2))
+        return potentialWinner;//diagonal
+      if(potentialWinner == fromString(1, 0) && potentialWinner == fromString(1, 2))
+        return potentialWinner;//vertical
+      if(potentialWinner == fromString(0, 1)&& potentialWinner == fromString(2, 1))
+        return potentialWinner;//horizontal
+    }
+    
+    potentialWinner = fromString(2, 2);
+    if(potentialWinner != null){
+      if(potentialWinner == fromString(2, 0) && potentialWinner == fromString(2, 1))
+        return potentialWinner;//vertical
+      if(potentialWinner == fromString(0, 2)&& potentialWinner == fromString(0, 1))
+        return potentialWinner;//horizontal
+    }
+    return null;
+  }
   
-  Player getCurrentPlayer(){}
   
-  ArrayList<String> getCurrentMoves(){}
+  Player getCurrentPlayer(){
+    return current;
+  }
+  
+  ArrayList<String> getCurrentMoves(){
+    ArrayList<String> result = new ArrayList<String>();
+    for(int i = 0; i<state.length; i++)
+      for(int j = 0; j<state.length; j++)
+        if(state[i][j].equals(" "))
+          result.add(i+" "+j);
+    return result;
+  }
   
   void makeMove(String move){}
   
   String toString(){}
+  
+  
+  
+  //helper methods
+  private Player fromString(String s){
+    if(s.toUpperCase().equals("X")
+       return x;
+    if(s.toUpperCase().equals("O")
+       return o;
+    return " ";//a blank string
+  }
+  private void togglePlayers(){
+    if(current == x)
+      current = o;
+    else if(current == o)
+      current = x;
+    else System.out.println("error with player names");
+  }
+  private Player fromString(int a, int b){
+    return fromString(state[a, b]);
+  }
 }
