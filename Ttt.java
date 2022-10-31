0be6c55ba93770b1cdd8f6744b26c678a07f8ed5
Timothy Lou <70876288+TimatoPaste@users.noreply.github.com>
2022-10-31 15:29:03 -0700

    Update Ttt.java

--- Ttt.java
+++ Ttt.java
@@ -1,5 +1,17 @@
 public class Ttt implements GameState{
-  boolean isGameOver(){}
+  private int[] latestMove;
+  boolean isGameOver(){
+    if(getWinner() == null){
+      return false;
+    }
+    //checks for draws
+    for(String a:state){
+      if(a.equals(" ")){
+        return false;
+      }
+    }
+    return true;
+  }
   
   Player getWinner(){}
   
@@ -7,7 +19,26 @@ public class Ttt implements GameState{
   
   ArrayList<String> getCurrentMoves(){}
   
-  void makeMove(String move){}
+  int[] stringMoveToIntMove(String move){
+    String[] moveS = move.split(" ");
+    int[] moveI = {Integer.parseInt(moveS[0]),Integer.parseInt(moveS[1])};
+    return moveI;
+  }
+  
+  void makeMove(String move){
+    latestMove = stringMoveToIntMove(move);
+    if(state[latestMove[0],latestMove[1]].equals(" ")){
+      if(getCurrentPlayer() == x){
+        state[latestMove[0],latestMove[1]] = "x";
+      }
+      if(getCurrentPlayer() == o){
+        state[latestMove[0],latestMove[1]] = "o";
+      }
+    }
+    else{
+      System.out.println("NOOOOOOOOOOOOOOOOOOO");//test
+    }
+  }
   
   String toString(){}
 }
